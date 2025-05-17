b0ca4cf0f103b7ca04a2772ed2085e7916dde054
ganesh kore <ganeshgannu711@gmail.com>
2025-05-17 07:00:19 +0530

    Next.js

---
+++ Next.js/my-app/app/about/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div> 
+        <h1>starting of Next.js</h1>
+        <ul>
+            <li>File Based routing</li>
+            <li>Server Components</li>
+            <li>Static Site Generation</li>
+            <li>Server Side Rendering</li>
+            <li>API Routes</li>
+            </ul>
+    </div>
+  )
+}
+
+export default page
