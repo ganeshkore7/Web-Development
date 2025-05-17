ae34cd26f17f48469ba25a02f711c9b602a06b19
ganesh kore <ganeshgannu711@gmail.com>
2025-05-17 16:55:25 +0530

    server components

---
+++ Next.js/server components/my-app/app/page.js
@@ -0,0 +1,18 @@
+// "use client";
+import React from "react";
+// import { useState } from "react";
+import Navbar from "@/components/Navbar";
+export default function Home() {
+  // const [count, setcount] = useState(0);
+  return (
+    <div>
+      <Navbar/>
+      i am a component 
+      {/* {count}
+      <button
+        onClick={ () => 
+          setcount(count + 1)}>Click me
+          </button> */}
+    </div>
+  )
+}
