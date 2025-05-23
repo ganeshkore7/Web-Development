8da430e2cc1f89ab12414bcb76c4ad551a62238f
ganesh kore <ganeshgannu711@gmail.com>
2025-05-23 17:27:28 +0530

    Authentication in next.js

---
+++ Next.js/Auth.js/auth.js/app/page.js
@@ -0,0 +1,17 @@
+"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
+import { use } from "react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if(session) {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+  return <>
+    Not signed in <br/>
+    <button onClick={() => signIn()}>Sign in</button>
+  </>
+}
